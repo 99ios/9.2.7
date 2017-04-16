4aa86190a5682c83ee48121743768f0410f4d14c
test <422538722@qq.com>
2017-04-16 15:12:20 +0800

    9.2.7
     导航栏设置背景图片

---
+++ 9.2.7 导航栏设置背景图片/9.2.7 导航栏设置背景图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  9.2.7 导航栏设置背景图片
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 9.2.7 导航栏设置背景图片/9.2.7 导航栏设置背景图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  9.2.7 导航栏设置背景图片
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
